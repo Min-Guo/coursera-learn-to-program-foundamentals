2c2e903cbe7614d27daa523eab258d47b5a1aab0
Min Guo <xmguomin@gmail.com>
2014-06-17 19:16:26 -0400

    Update function board_contain_word

--- assignment-3/a3.py
+++ assignment-3/a3.py
@@ -147,6 +147,9 @@ def board_contains_word(board, word):
     >>> board_contains_word([['A', 'N', 'T', 'T'], ['X', 'S', 'O', 'B']], 'ANT')
     True
     """
+    if board_contains_word_in_row(board, word) == True or board_contains_word_in_column(board, word) == True:
+        return True
+    return False
 
 
 def word_score(word):
